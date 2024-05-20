7b150b398d9204797dab68ed5c331e020c509c35
Moustapha S. Dieme <ndieme16@hotmail.com>
2024-05-20 04:49:43 +0200

    init project

---
+++ src/main/java/com/jcdecaux/devskill/DevskillApplication.java
@@ -0,0 +1,13 @@
+package com.jcdecaux.devskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevskillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcdecaux/devskill/DevskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcdecaux.devskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
